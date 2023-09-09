39be4fae6d8418ae26093c439f204dd3bb6dbc27
GuiDev182 <guilhermesemusa@gmail.com>
2023-09-09 13:29:34 -0300

    exercicio 07

---
+++ mensagem20x.java
@@ -0,0 +1,9 @@
+public class mensagem20x {
+    public static void main(String[] args) {
+        String mensagem = "Eu gosto de estudar algoritmos";
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(mensagem);
+        }
+    }
+}
