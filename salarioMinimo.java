43fb796dfcdb33bf18026077b54582e4accecae4
GuiDev182 <guilhermesemusa@gmail.com>
2023-09-09 13:19:37 -0300

    exercicio 5

---
+++ salarioMinimo.java
@@ -0,0 +1,8 @@
+public class salarioMinimo {
+    public static void main(String[] args) {
+        double salarioMinimo = 788.00;
+        double salario = 1650.00;
+        double resultado = salario / salarioMinimo;
+        System.out.println("O salário é " + resultado + " salários mínimos");
+    }
+}
