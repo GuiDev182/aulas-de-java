a6152c88acccdc5213d7601fcd8143c40e94ba94
GuiDev182 <guilhermesemusa@gmail.com>
2023-09-09 11:55:46 -0300

    exercicio 03

---
+++ informarSaldo.java
@@ -0,0 +1,13 @@
+
+public class informarSaldo {
+   public static void main(String[] args) {
+    double saldo = 1000;
+    double reajuste = (saldo * 0.01);
+    System.out.println("Seu saldo é: " + saldo);
+
+    double saldoTotal = saldo + reajuste;
+    System.out.println("Seu saldo reajustado é: " + saldoTotal);
+  
+
+   }
+}
