46aa35816e9203f7e1f0c93bc309e0654ad846b9
GuiDev182 <guilhermesemusa@gmail.com>
2023-09-10 17:58:46 -0300

    exercicio 10

---
+++ nomeNumero.java
@@ -0,0 +1,11 @@
+/*Leia o nome um número do usuário um número N e escreva o nome dele na tela N
+vezes. */
+public class nomeNumero {
+    public static void main(String[] args) {
+        String nome = "João";
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.println(nome);
+        }
+    }
+}
