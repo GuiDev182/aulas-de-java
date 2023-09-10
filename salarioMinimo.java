9528202b5d36c7cdaf0d0ddd8b53f53598c41e7b
GuiDev182 <guilhermesemusa@gmail.com>
2023-09-10 18:12:54 -0300

    exercicio 05

--- salarioMinimo.java
+++ salarioMinimo.java
@@ -1,3 +1,6 @@
+/*Crie um algoritmo que leia o valor do salário mínimo e o valor do salário de um
+usuário, calcule a quantidade de salários mínimos que esse usuário ganha e
+imprima o resultado. (1SM=R$788,00) */
 public class salarioMinimo {
     public static void main(String[] args) {
         double salarioMinimo = 788.00;
