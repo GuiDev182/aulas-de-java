4f007f8b7de2baf740fe407ed633b3596f1d351a
GuiDev182 <guilhermesemusa@gmail.com>
2023-09-10 17:55:12 -0300

    exercicio 9

---
+++ nomeUsuario.java
@@ -0,0 +1,12 @@
+/**
+ Leia o nome do usuário e escreva o nome dele na tela 10 vezes.
+ */
+public class nomeUsuario {
+    public static void main(String[] args) {
+        String nome = "João";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(nome);
+        }
+    }
+    
+}
