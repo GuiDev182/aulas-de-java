c038374bae8004bca463fab1714150fa47c53e94
GuiDev182 <guilhermesemusa@gmail.com>
2023-09-11 19:49:28 -0300

    exercicio 02

--- mediaAritmetica1.java
+++ mediaAritmetica1.java
@@ -3,25 +3,25 @@ dos números 4, 5 e 6. A soma das duas médias. A média das médias.*/
 public class mediaAritmetica1 {
 
     public static void main(String[] args) {
-        final int a = 8;
-        final int b = 9;
-        final int c = 7;
-        final int d = 4;
-        final int e = 5;
-        final int f = 6;
+        float  a = 8;
+        float  b = 9;
+        float  c = 7;
+        float  d = 4;
+        float  e = 5;
+        float  f = 6;
 
-        int media = (a + b + c) / 3;
+        float media = (a + b + c) / 3;
 
         System.out.println("a media aritmetica de " + a + ", " + b + " e " + c + " é: " + media);
 
-        int media2 = (d + e + f) / 3;
+        float media2 = (d + e + f) / 3;
         
         System.out.println("a media aritmetica de " + d + ", " + e + " e " + f + " é: " + media2);
 
-        int soma = media + media2;
+        float soma = media + media2;
         System.out.println("a soma das medias é: " + soma);
 
-        int media3 = soma / 2;
+        float media3 = soma / 2;
         System.out.println("a média das médias é: " + media3);
     }
 
